245409c898e73e8d353a69f77463f9f3d693d9f8
hank9999 <hank9999@qq.com>
2020-07-27 13:27:00 +0800

    Optimize code style

--- plugins/say.py
+++ plugins/say.py
@@ -3,6 +3,7 @@ from mcrcon_setting import rcon_setting
 from nonebot import on_command, CommandSession
 from nonebot import permission as perm
 
+
 @on_command('say', permission=perm.GROUP_MEMBER, only_to_me=False)
 async def say(session: CommandSession):
     chat_data = str(session.current_arg_text).strip()
@@ -15,11 +16,14 @@ async def say(session: CommandSession):
     else:
         try:
             username = chat_data[:chat_data.find(' ')]
-            text = chat_data[chat_data.find(' ')+1:]
-            await rcon('tellraw @a {"extra":[{"text":"§a"},{"clickEvent":{"action":"suggest_command","value":"[QQ]' + username + '"},"text":"§f[QQ] ' + username + '§r: "},{"text":"§f"},{"text":"§f' + text + '"}],"text":""}')
+            text = chat_data[chat_data.find(' ') + 1:]
+            await rcon(
+                'tellraw @a {"extra":[{"text":"§a"},{"clickEvent":{"action":"suggest_command","value":"[QQ]' + username + '"},"text":"§f[QQ] ' + username + '§r: "},{"text":"§f"},{"text":"§f' + text + '"}],"text":""}'
+            )
         except Exception:
             message = '参数不全 获取帮助 #say help'
             await session.send(message)
+
 
 async def rcon(_command: str) -> str:
     with MCRcon(rcon_setting['address'], rcon_setting['password'], rcon_setting['port']) as mcr:
